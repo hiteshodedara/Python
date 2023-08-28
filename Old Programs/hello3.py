d88ff0f7be09ff74bd5517e27544eb89e31dc772
hiteshodedara <hiteshodedara1010@gmail.com>
2023-08-28 19:09:10 +0530

    add new emoji printing program

---
+++ Old Programs/hello3.py
@@ -0,0 +1,11 @@
+for i in range(5):
+        print("\U0001F923"*i)
+print
+i=5
+# for k in range():
+#         print("\U0001F923",end="")
+
+for j in range(i):
+        i=i-1
+        j=j-1
+        print("\U0001F923"*i)
