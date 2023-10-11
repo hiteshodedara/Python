c7a49c0866cac4fc7b7d9a00b8ab69363d27011e
hiteshodedara <hiteshodedara1010@gmail.com>
2023-10-11 11:16:14 +0530

    fix some problems

---
+++ new Programs/testai.py
@@ -0,0 +1,23 @@
+import openai
+
+# Set your API key
+api_key = 'your-api-key-here'
+
+# Initialize the OpenAI API client
+openai.api_key = api_key
+
+# Your prompt (the text you want the model to continue)
+prompt = "Translate the following English text to French:"
+
+# Make a request to the API
+response = openai.Completion.create(
+    engine="text-davinci-002",  # You can choose a different engine if needed
+    prompt=prompt,
+    max_tokens=50,  # Adjust this based on your desired response length
+)
+
+# Extract the generated text from the API response
+generated_text = response.choices[0].text
+
+# Print the generated text
+print(generated_text)
